378ac4dca68f85fadb68ae210d5812eea853c8b4
nims92 <nimeshchandramaniya@gmail.com>
2022-12-04 12:14:09 +0530

    Added card scriptable object scripts

---
+++ IndianCardBattle/Assets/ScriptableObjects/Scripts/Card.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Card
+{
+    public int ID;
+    public string name;
+    public int power;
+    public int energyCost;
+    public int abilityID;
+    public Sprite cardImage;
+}
---
+++ IndianCardBattle/Assets/ScriptableObjects/Scripts/CardDatabase.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CardDatabase", menuName = "ScriptableObjects/CardDatabaseScriptableObject", order = 1)]
+public class CardDatabase : ScriptableObject
+{
+   
+}
