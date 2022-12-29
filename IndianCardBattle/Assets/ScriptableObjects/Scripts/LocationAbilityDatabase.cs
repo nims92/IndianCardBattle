586ae77e02782ecbbad72de811a4c20e2fde75b1
Nimesh Chandramaniya <>
2022-12-29 08:40:36 +0530

    Fixed problem with locations related SO files

--- IndianCardBattle/Assets/ScriptableObjects/Scripts/LocationAbilityDatabase.cs
+++ IndianCardBattle/Assets/ScriptableObjects/Scripts/LocationAbilityDatabase.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "LocationAbilityDatabase", menuName = "Scriptable Objects/Location Ability Database", order = 1)]
-public class LocationAbilityDatabase
+public class LocationAbilityDatabase : ScriptableObject
 {
     public List<LocationAbilityDataEntry> listOfLocationAbilities;
 }
--- IndianCardBattle/Assets/ScriptableObjects/Scripts/LocationDatabase.cs
+++ IndianCardBattle/Assets/ScriptableObjects/Scripts/LocationDatabase.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "LocationDatabase", menuName = "Scriptable Objects/Location Database", order = 1)]
-public class LocationDatabase
+public class LocationDatabase : ScriptableObject
 {
     public List<LocationDataEntry> locationList;
 }
