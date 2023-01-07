26c39a18f51089b76bf81a05e092845754ee9f39
Nimesh Chandramaniya <>
2023-01-07 12:54:45 +0530

    - GameLoader events removed as not used - Opponent card hand area set hidden

--- IndianCardBattle/Assets/Scripts/Events/GameLoaderEvents.cs
+++
@@ -1,7 +0,0 @@
-public static class GameLoaderEvents
-{
-    public const string LOCATIONS_SPAWNED = "LocationsSpawned";
-    public const string PLAYER_CARDS_SPAWNED = "PlayerCardsSpawned";
-    public const string OPPONENT_CARDS_SPAWNED = "OpponentCardsSpawned";
-    
-}
--- IndianCardBattle/Assets/Scripts/Events/TurnEvents.cs
+++ IndianCardBattle/Assets/Scripts/Events/TurnEvents.cs
@@ -1,6 +1,5 @@
 public static class TurnEvents
 {
     public const string TURN_UPDATED = "TurnUpdated";
-   
     public const string CURRENT_TURN_ENDED = "CurrentTurnEnded";
 }
